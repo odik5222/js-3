f94bbcc52afd87623f4d1cbf18dc0f8c4cdf3def
odik_web <116338011+odik5222@users.noreply.github.com>
2024-10-01 20:17:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const numbers = [1, 5, 3, 9, 2,12];
+
+const number = numbers.map(num => num);
+
+const max = Math.max(...number);
+
+console.log(max); 
+
+
+
+const numberss = [1, 5, 3, 9, 2,12];
+
+const number2 = numberss.map(num1 => num1);
+
+const min2 = Math.min(...number2);
+
+console.log(min2); 
